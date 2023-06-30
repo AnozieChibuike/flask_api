7dc80e16605bf900c0f017c930761abaefcc69b5
Anozie Joel Chibuike <chibuikeanozie0@gmail.com>
2023-06-30 20:12:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 
+book_list = [
+    {'id': 1,'title': 'Things fall apart','author':'Chinua Achebe'},
+    {'id': 2,'title': 'Poor dad Rich dad','autho':'John Neeman'},
+    ]
+
 @app.route('/books',methods=['GET','POST')
 def books():
+    if request.method == 'GET':
+        if book_list:
+            return jsonify(book_list)
     
